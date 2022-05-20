b74c12dbd5bc3fc30f4e8bab6f11250f17ed63f8
hide <hide09721@gmail.com>
2022-05-20 14:17:14 +0900

    first commit

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
