2dd49d0844c3ecbe95d374cf4bd3d8eff8f9832b
hide <hide09721@gmail.com>
2022-05-31 19:41:39 +0900

    新規登録のエラー修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,23 +27,17 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('/', 'HomeController@index')->name('home');
 
     Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
-
     Route::post('/folders/create', 'FolderController@create');
 
     Route::group(['middleware' => 'can:view,folder'], function() {
         Route::get('/folders/{folder}/tasks', 'TaskController@index')->name('tasks.index');
         
         Route::get('/folders/{folder}/tasks/create', 'TaskController@showCreateForm')->name('tasks.create');
-    
         Route::post('/folders/{folder}/tasks/create', 'TaskController@create');
     
         Route::get('/folders/{folder}/tasks/{task_id}/edit', 'TaskController@showEditForm')->name('tasks.edit');
-    
         Route::post('/folders/{folder}/tasks/{task_id}/edit', 'TaskController@edit');
     });
-
-
-
 
 });
 
